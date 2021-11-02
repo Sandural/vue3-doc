1102cb86044e69178061949203766df68406cda1
rick <1370365840@qq.com>
2021-11-03 01:05:39 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Todo from './components/Todo.vue'
+
+</script>
+
+<template>
+    <Todo #default="{ data }">
+        <i class="fa-check-circle" :class="data.isDone ? 'fas' : 'far'"></i>
+        <span class="ml-1">{{data.title}}</span>
+    </Todo>
+</template>
+
+<style>
+    #app {
+        margin-top: 30px;
+        margin-left: 30px;
+    }
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,28 @@
+<template>
+    <ul>
+        <li v-for="item in todos" :key="item.id"
+            @click="changeItemStatus(item.id)"
+            class="p-1 mb-1 px-2 cursor-pointer"
+            :class="item.isDone ? 'bg-red-100' : ''">
+            <slot :data="item"></slot>
+        </li>
+    </ul>
+</template>
+
+<script setup>
+import {reactive} from 'vue'
+
+const todos = reactive([
+    {id: 1, title: '办理业务', isDone: false},
+    {id: 2, title: '完成家庭作业', isDone: true},
+    {id: 3, title: '上计算机课', isDone: false},
+])
+
+const changeItemStatus = (id) => {
+    todos.filter(v => v.id === id).map(j => j.isDone = !j.isDone)
+}
+</script>
+
+<style scoped>
+
+</style>
